4059d58fb38552734ee855ed5336d4cbf9254088
mcilhaggis <rachael.mcilhagga@live.co.uk>
2020-11-30 17:04:37 -0500

    adding formatting to README file generated

--- index.js
+++ index.js
@@ -2,57 +2,60 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-
-//Create a forEach loop for every section filled to create the table of contents?
-  function generateTemplate(data){
-      return `# Project Title = ${data.Title}
-      Project Description = ${data.Description}`
-
-  }  
-
-
-// function to initialize program
+// function to initialize program and ask the questions
 function init() {
     inquirer.prompt([
         {
             type:'input',
-            name:'Title',
+            name:'title',
             message:'What is the title of this project?'
         },
         {
             type:'input',
-            name:'Description',
+            name:'description',
             message:'Give a brief description of this project:'
         },
         {
-            type:'input',
-            name:'Installation',
-            message:'Detail how to install the project:'
-        },
+            type:'confirm',
+            name:'installation',
+            message:'Does your project require installation?',
+        }, {
+            when: function (response) {
+               return response.installation;
+        },      name: 'installationInstructions',
+                type:'input',
+                message:'Give a brief description of steps to install:'
+            },
         {
             type:'input',
-            name:'Usage',
+            name:'usage',
             message:'Detail how to use the project:'
         },
         {
-            type:'input',
-            name:'Licensing',
-            message:'What is the name of the license that this work is published under?'
+            type:'list',
+            name:'licensing',
+            message: 'Which license is this project published under?',
+            choices: ['MIT', 'GPL V3', 'AGPL']
         },
         {
             type:'input',
-            name:'Contributing',
-            message:'Who are the contributors to this project?'
+            name:'contributing',
+            message:'What are the contributors names for this project?'
         },
         {
             type:'input',
-            name:'Tests',
+            name:'tests',
             message:'Write what tests should be done:',//is that the corectthing to ask?
         },
         {
             type:'input',
-            name:'Questions',
-            message:'Provide an email for people to get in touch regarding the project:'
+            name:'github',
+            message:'Provide your Github username:'
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'Provide your email address:'
         }
     
     ]).then(function(answers){
@@ -64,6 +67,43 @@ function init() {
     
     }) 
 }
+
+
+
+//function to opulate and stle the file correctly 
+function generateTemplate(data){
+return `# Project Title: ${data.title} 
+https://img.shields.io/badge/License%3A-${data.licensing}-blue.svg
+## Project Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Licensing](#licensing)
+* [Contributions](#contributions)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${data.installationInstructions}
+
+## Usage
+${data.usage}
+
+## Licensing 
+The licensing used for this project is ${data.licensing}
+
+## Contributions 
+${data.contributing}
+    
+## Tests
+${data.tests}
+    
+## Questions
+* If you have any questions about this project, please reach out to me  through <a href="https://github.com/${data.github}">Github</a>  or via <a href="mailto:${data.email}">Email</a>`
+
+}  
 
 // function call to initialize program
 init();
